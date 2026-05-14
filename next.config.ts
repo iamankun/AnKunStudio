6ca5a2578cfb45517403be502e6f5f7f3526a29f
An Kun <ankun.n.m@gmail.com>
2026-05-14 21:31:56 +0700

    main

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const isDev = process.env.NODE_ENV !== "production";
 
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
   },
   typescript: {
     ignoreBuildErrors: true,
